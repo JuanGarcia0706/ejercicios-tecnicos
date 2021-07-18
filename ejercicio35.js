8c2eef55a5be7a99c4ffc5464ea230f51ad53a3e
Juan Garcia <juan.garcia070699@gmail.com>
2021-07-18 12:46:58 -0300

    Added 35.js

---
+++ ejercicio35.js
@@ -0,0 +1,5 @@
+// Dado un array devolver otro con el primer y ultimo valor del array original
+
+const primerUltimo = (arr) => [arr[0], arr[arr.length - 1]];
+
+console.log(primerUltimo([100, 200, 300, 780]));
